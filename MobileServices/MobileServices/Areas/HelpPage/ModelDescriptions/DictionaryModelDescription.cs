3b9ea0a644fa99bf6e44b2492f386ff633f365e3
mmuratgurses <38799102+mmuratgurses@users.noreply.github.com>
2023-03-15 09:44:22 +0300

    Initial commit

---
+++ MobileServices/MobileServices/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MobileServices.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MobileServices/MobileServices/Controllers/ATMController.cs
@@ -0,0 +1,40 @@
+﻿using ATMDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MobileServices.Controllers
+{
+    public class ATMController : ApiController
+    {
+        [Route("api/GetProblems")]
+        [System.Web.Http.HttpGet]
+        public IEnumerable<PROBLEM_MANAGEMENT> Get() 
+        {
+            using (ATMEntities entities = new ATMEntities()) 
+            {
+                return entities.PROBLEM_MANAGEMENT.ToList();
+            }
+        }
+
+        [Route("api/GetProblem")]
+        [System.Web.Http.HttpGet]
+        public List<PROBLEM_MANAGEMENT> Get([FromBody] Parameters parameters)
+        {
+            using (ATMEntities entities = new ATMEntities())
+            {
+                return entities.PROBLEM_MANAGEMENT.Where(p => p.TERMINAL_ID==parameters.TerminalID && p.PROBLEM_START_TIME>=DateTime.Now ).ToList() ;
+            }
+        }
+
+    }
+
+    public class Parameters
+    {
+        public int Day { get; set; }
+        public string TerminalID { get; set; }
+    }
+}
